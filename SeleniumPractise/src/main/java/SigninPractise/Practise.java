d658be52b95a3bfa51d1f039d3f4b8304ec09e4c
Khitish1995 <khitishkumarjena@gmail.com>
2025-06-24 22:17:10 +0530

    Static Dropdown Test done

--- SeleniumPractise/src/main/java/SigninPractise/Practise.java
+++ SeleniumPractise/src/main/java/SigninPractise/Practise.java
@@ -20,19 +20,35 @@ public class Practise {
 		driver.get("https://rahulshettyacademy.com/AutomationPractice");
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@id='autocomplete']")).click();
-		driver.findElement(By.xpath("//input[@id='autocomplete']")).sendKeys("Ind");
+//		driver.findElement(By.xpath("//input[@id='autocomplete']")).click();
+//		driver.findElement(By.xpath("//input[@id='autocomplete']")).sendKeys("Ind");
+//		Thread.sleep(2000);
+//		List<WebElement> listCountry = driver.findElements(By.xpath("//li[@class='ui-menu-item']"));
+//		
+//		String target = "India";
+//		
+//		for(int i=0;i<listCountry.size();i++) {
+//			if(listCountry.get(i).getText().equals(target)) {
+//			listCountry.get(i).click();
+//			System.out.println("Country Clicked");
+//			break;}
+//		}
+		
+		WebElement SSDropdown = driver.findElement(By.xpath("//select[@id='dropdown-class-example']"));
+		
+		Select dropdown = new Select(SSDropdown);
+		dropdown.selectByContainsVisibleText("Option1");
 		Thread.sleep(2000);
-		List<WebElement> listCountry = driver.findElements(By.xpath("//li[@class='ui-menu-item']"));
+		System.out.println("option 1 selected");
 		
-		String target = "India";
+		dropdown.selectByIndex(2);
+		Thread.sleep(2000);
+		System.out.println("option 2 selected");
 		
-		for(int i=0;i<listCountry.size();i++) {
-			if(listCountry.get(i).getText().equals(target)) {
-			listCountry.get(i).click();
-			System.out.println("Country Clicked");
-			break;}
-		}
+		dropdown.selectByValue("option3");
+		Thread.sleep(2000);
+		System.out.println("option 3 selected");
+		
 		System.out.println("Windows closed");
 		driver.close();
 	}
