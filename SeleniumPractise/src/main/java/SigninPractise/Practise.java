b507b2730fccd2eff15ab1603813229171cfe73a
Khitish1995 <khitishkumarjena@gmail.com>
2025-07-14 22:54:15 +0530

    Multi Windows opened

--- SeleniumPractise/src/main/java/SigninPractise/Practise.java
+++ SeleniumPractise/src/main/java/SigninPractise/Practise.java
@@ -20,137 +20,81 @@ public class Practise {
 		System.setProperty("Webdriver.chrome.driver", "C:\\Softwares\\SeleniumWebdriver\\ChromeDriver\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		
 		driver.get("https://rahulshettyacademy.com/AutomationPractice");
 		driver.manage().window().maximize();
-		
+		//Dynamic Dropdown
 		driver.findElement(By.xpath("//input[contains(@id,'autocomplete')]")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[contains(@id,'autocomplete')]")).sendKeys("Ind");
 		Thread.sleep(2000);
-		List<WebElement> countryList = driver.findElements(By.xpath("//li[contains(@class,'ui-menu-item')]"));
 		String country = "India";
+		List<WebElement> countryList = driver.findElements(By.xpath("//li[contains(@class,'ui-menu-item')]"));
 		for(int i=0;i<countryList.size();i++) {
 			if(countryList.get(i).getText().equalsIgnoreCase(country)) {
 				countryList.get(i).click();
-				System.out.println("Country clicked");
+				System.out.println("Clicked the country");
+				Thread.sleep(2000);
 				break;
 			}
 		}
 		
-		
-		WebElement staticDrpdown = driver.findElement(By.xpath("//select[contains(@id,'dropdown-class-example')]"));
-		
-		Select dropdown = new Select(staticDrpdown);
-		dropdown.selectByIndex(1);
-		System.out.println("option1 clicked");
+		//Static dropdown
+		WebElement dropdown = driver.findElement(By.xpath("//select[contains(@id,'dropdown-class-example')]"));
+		Select staticdrpdown = new Select(dropdown);
+		staticdrpdown.selectByIndex(1);
+		System.out.println("1st quota clicked");
 		Thread.sleep(2000);
 		
-		dropdown.selectByValue("option2");
-		System.out.println("option2 clicked");
+		staticdrpdown.selectByValue("option2");
+		System.out.println("2nd quota clicked");
 		Thread.sleep(2000);
 		
-		dropdown.selectByVisibleText("Option3");
-		System.out.println("option3 clicked");
+		staticdrpdown.selectByVisibleText("Option3");
+		System.out.println("3rd quota clicked");
 		Thread.sleep(2000);
-		
 		
 		driver.findElement(By.xpath("//input[contains(@id,'alertbtn')]")).click();
+		Thread.sleep(2000);
 		driver.switchTo().alert().accept();
 		System.out.println("Alert clicked");
-		Thread.sleep(2000);
 		
+		//switch Tab Example
 		driver.findElement(By.xpath("//a[contains(@id,'opentab')]")).click();
-		Set<String> windowwHandle = driver.getWindowHandles();
-		Iterator<String> itr= windowwHandle.iterator();
 		
-		String parentID = itr.next();
-		String childID = itr.next();
+		Set<String> windowhandles = driver.getWindowHandles();
+		Iterator<String> jump = windowhandles.iterator();
 		
-		driver.switchTo().window(childID);
-		Thread.sleep(3000);
+		String parentid = jump.next();
+		String childid = jump.next();
+		
+		driver.switchTo().window(childid);
 		String childtitle = driver.getTitle();
+		Thread.sleep(2000);
 		System.out.println(childtitle);
 		
-		driver.switchTo().window(parentID);
-		String pareenttitle = driver.getTitle();
-		System.out.println(pareenttitle);
-		Thread.sleep(3000);
-		/*String Date ="9";
-		String Month ="October";
-		String Year = "2026";
-		
-		driver.findElement(By.xpath("//div[contains(@class,'react-date-picker__inputGroup')]")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//button[contains(@class,'react-calendar__navigation__label')]")).click();
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//button[contains(@class,'react-calendar__navigation__label')]")).click();
-		Thread.sleep(1000);
-		
-		driver.findElement(By.xpath("//button[text()='"+Year+"']")).click();
+		driver.switchTo().window(parentid);
+		String parenttitle = driver.getTitle();
 		Thread.sleep(2000);
+		System.out.println(parenttitle);
+		System.out.println("Switch window started");
+		System.out.println();
+		//switch window Example
 		
-		List<WebElement> monthList = driver.findElements(By.xpath("//button[contains(@class,'react-calendar__tile react-calendar__year-view__months__month')]"));
-		for(int i=0;i<monthList.size();i++) {
-			if(monthList.get(i).getText().equalsIgnoreCase(Month)) {
-				monthList.get(i).click();
-				System.out.println("Month clicked");
-				break;
-			}
-		}
+		driver.findElement(By.xpath("//button[contains(@id,'openwindow')]")).click();
 		
-		Thread.sleep(1000);
+		Set<String> windowjump = driver.getWindowHandles();
+		Iterator<String> itr = windowjump.iterator();
+		String parentttwindow = itr.next();
+		String childwindow = itr.next();
 		
-//		driver.findElements(By.xpath("//button[contains(@class,'react-calendar__tile react-calendar__month-view__days__day')]")).get(Integer.parseInt(Date)+2).click();
-//		Thread.sleep(1000);
-		
-		List<WebElement>dateList = driver.findElements(By.xpath("//button[contains(@class,'react-calendar__tile react-calendar__month-view__days__day')]"));
-		for(int j=0;j<dateList.size();j++) {
-			if(dateList.get(j).getText().equalsIgnoreCase(Date)) {
-				dateList.get(j).click();
-				System.out.println("Date clicked");
-				break;
-			}
-		} */
-		/*
-		//---------------Auto-suggestiveDRopdown----------------------------
-		driver.findElement(By.xpath("//input[contains(@id,'autocomplete')]")).click();
+		driver.switchTo().window(childwindow);
+		String childtitlewindow = driver.getTitle();
+		System.out.println(childtitlewindow);
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[contains(@id,'autocomplete')]")).sendKeys("Ind");
-		Thread.sleep(2000);
+		driver.switchTo().window(parentttwindow);
+		String parenttitlewindow = driver.getTitle();
+		System.out.println(parenttitlewindow);
 		
-		List<WebElement> countryList = driver.findElements(By.xpath("//li[@class='ui-menu-item']"));
-		String country = "India";
-		for(int i=0;i<countryList.size();i++) {
-			if(countryList.get(i).getText().equalsIgnoreCase(country)) {
-				countryList.get(i).click();
-				System.out.println("Country clicked");
-				break;
-			}
-		}
-		
-		//---------------StaticDRopdown----------------------------
-		
-		WebElement drpDown = driver.findElement(By.xpath("//select[contains(@id,'dropdown-class-example')]"));
-		Select staticDrpdown = new Select(drpDown);
-		staticDrpdown.selectByIndex(1);
-		System.out.println("1ST OPTION CLICKED");
-		Thread.sleep(2000);
-		
-		staticDrpdown.selectByValue("option2");
-		System.out.println("2ND OPTION CLICKED");
-		Thread.sleep(2000);
-		
-		staticDrpdown.selectByVisibleText("Option3");
-		System.out.println("3RD OPTION CLICKED");
-		Thread.sleep(2000);
-		
-		//---------------Alert----------------------------
-		
-		driver.findElement(By.xpath("//input[contains(@id,'alertbtn')]")).click();
-		Thread.sleep(2000);
-		driver.switchTo().alert().accept();
-		System.out.println("Alert accepted CLICKED");  */
 		
 		Thread.sleep(2000);
 		driver.quit();
