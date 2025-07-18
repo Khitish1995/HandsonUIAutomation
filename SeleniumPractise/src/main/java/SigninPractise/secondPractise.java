75372d95bf22beb68d769e6b4251b7e25c2585f3
Khitish1995 <khitishkumarjena@gmail.com>
2025-07-18 19:30:05 +0530

    Synchronisation - Implicit/Explicit wait

--- SeleniumPractise/src/main/java/SigninPractise/secondPractise.java
+++ SeleniumPractise/src/main/java/SigninPractise/secondPractise.java
@@ -1,45 +1,24 @@
 package SigninPractise;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 public class secondPractise {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("Webdriver.chrome.driver", "C:\\Softwares\\SeleniumWebdriver\\ChromeDriver\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-
-		String[] itemsNeeded= {"Cucumber","Brocolli","Beetroot"};
-		driver.get("https://rahulshettyacademy.com/seleniumPractise");
-		driver.manage().window().maximize();
 		
-		
-		List<WebElement> veggiesList = driver.findElements(By.xpath("//h4[@class='product-name']"));
-		int j=0;
-		for(int i=0;i<veggiesList.size();i++) {
-			String[] name = veggiesList.get(i).getText().split("-");
-			String veggiesName = name[0].trim();
-			
-			List itemsNeededList = Arrays.asList(itemsNeeded);
-			if(itemsNeededList.contains(veggiesName)) {
-				j++;
-				driver.findElements(By.xpath("//div[@class='product-action']")).get(i).click();
-				System.out.println("Clicked this " +veggiesName+ " Successfully");
-				if(j==itemsNeeded.length) {
-					break;
-				}
-			}
-		}
-		
-		
-		Thread.sleep(2000);
-		driver.quit();
+		//Thread.sleep(2000);
+		//driver.quit();
 		
 	}
 
